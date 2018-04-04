eb318c8a751a61a3f1429511c09d8ba6a0c668e4
flexedor <fedor6574022@gmail.com>
2018-04-05 00:26:08 +0300

    to do list

--- toDo.list.js
+++ toDo.list.js
@@ -1,27 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function()
+{
     var field = document.getElementById('todo_list');
 
-    document.getElementById('add').onclick=function () {
-        var new_todo =document.getElementById('new_todo').value;
-        if (new_todo === '') {
-            alert("Вот ты ленивый !У тебя что вообще нет дел!");
-        } else {
-        document.getElementById("new_todo").value = "";
-        builder(new_todo);
-    }};
-    function builder(new_todo) {
-        var li = document.createElement("li");
-        field.appendChild(li);
-        li.innerHTML =new_todo;
-    }
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
+        document.getElementById('add').onclick=function ()
+        {
+            var new_todo =document.getElementById('new_todo').value;
+            if (new_todo === '') {
+                alert("Вот ты ленивый !У тебя что вообще нет дел!");
+            } else {
+            document.getElementById("new_todo").value = "";
+            builder(new_todo);
+        }};
 
-    }
-}, false);
-// for ( )
+
+        function builder(new_todo)
+        {
+            var li = document.createElement("li");
+            var span = document.createElement("SPAN");
+            var br =document.createElement("br");
+
+            field.appendChild(li);
+            li.innerHTML =new_todo;
+            li.appendChild(br);
+
+            li.appendChild(span);
+            span.innerHTML='delete';
+        }
+        var list = document.querySelector('ul');
+        list.addEventListener('click', function(ev)
+        {
+            if (ev.target.tagName === 'LI') {
+                ev.target.classList.toggle('checked');
+
+            }else if (ev.target.tagName ==="SPAN"){
+                ev.target.parentNode.style.display = "none";
+
+            }
+        }, false);
+
+
 document.getElementById("delete").onclick=function () {
 alert("минус дело !");
 var elements = document.getElementsByClassName('checked');
